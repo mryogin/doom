d452b5c5dc07b6909cd738c745c7fb4c2b1486b9
mr_yogin <gvenkun@gmail.com>
2023-05-05 18:44:30 -0400

    Make static sprite

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from map import *
 from player import *
 from raycasting import *
 from object_renderer import *
+from sprite_objects import *
 
 
 class Game:
@@ -21,11 +22,13 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRender(self)
         self.raycasting = RayCasting(self)
+        self.static_sprite = SpriteObject(self)
 
 
     def update(self):
         self.player.update()
         self.raycasting.update()
+        self.static_sprite.update()
         pg.display.flip() #отображаем экран
         self.delta_time = self.clock.tick(FPS) #получаем дельту времени
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
